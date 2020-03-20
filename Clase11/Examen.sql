a60e771b5ccd3e47d111548347aed6521cc73e3f
Diego Serrano <diegojserrano@gmail.com>
2020-03-20 18:33:44 -0300

    Agregados los scripts de las base de datos

---
+++ Clase11/Examen.sql
@@ -0,0 +1,17 @@
+create table Articulos (
+    IdArticulo int identity(1,1) primary key,
+    Nombre varchar(50) not null,
+    Precio real not null
+)
+
+GO
+
+create table Compras (
+    IdCompra int identity(1,1) primary key,
+    IdArticulo int not null,
+    Cantidad int not null,
+    FOREIGN key (IdArticulo) references Articulos(IdArticulo)
+)
+GO
+
+
---
+++ Clase11/Personas.sql
@@ -0,0 +1,8 @@
+create database Prueba
+GO
+create table Personas (
+    Id int IDENTITY(1,1) PRIMARY key,
+    Nombre varchar(50) not null
+)
+
+GO
